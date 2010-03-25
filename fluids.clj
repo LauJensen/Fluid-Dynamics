033f2137dd296d69c218b241207e1540e156a613
Lau B. Jensen <lau.jensen@bestinclass.dk>
2010-03-25 22:02:37 +0100

    Added hints and removed defworker.
    Density step now runs at 0.008s

--- fluids.clj
+++ fluids.clj
@@ -36,49 +36,35 @@
 	     (recur (unchecked-inc ~'j))))
 	 (recur (unchecked-inc ~'i))))))
 
-(defmacro map! [f lookup collection]
-  `(let [[w# h#] [(count ~lookup) (-> ~lookup first count)]]
-     (let [board# (make-array Double/TYPE h# w#)]
-       (do-board [w# h#]       
-	    (let [~'above  (aget! ~'doubles ~lookup (unchecked-dec ~'i) ~'j)
-		  ~'below  (aget! ~'doubles ~lookup (unchecked-inc ~'i) ~'j)
-		  ~'left   (aget! ~'doubles ~lookup ~'i (unchecked-dec ~'j))
-		  ~'right  (aget! ~'doubles ~lookup ~'i (unchecked-inc ~'j))
-		  ~'self   (aget! ~'doubles ~lookup ~'i ~'j)]
-	      (aset! ~'doubles board# (int ~'i) (int ~'j) ~f)))
-       board#)))
-
 (declare diffuse-board)
 
 (def *dt* 1.0)
 
-(defn make-densities [w h]
-  (vec (map vec (partition h (vec (repeat (* w h) 0))))))
-
-(defn make-densities [w h]
+(defn new-array [w h]
   (let [board (make-array Double/TYPE h w)]
-    (dotimes [i h]
-      (dotimes [j w]
-	(aset! doubles board i j 0)))
+    (dotimes [i w]
+      (dotimes [j h]
+	(aset! doubles board i j 0.0)))
     board))
 
-(defn new-array [w h]
-  (let [array (make-array Integer/TYPE h w)]
-    (do-board [w h] (aset! ints array i j 0))
-    array))
+(defn up-array [orig]
+  (let [[u v] orig
+	[w h] [(count v) (-> v first count)]]
+    (do-board [w h]  (aset! doubles v i j -0.5))
+    [u v]))
 
 (defn topdown-array [orig]
   (let [[u v] orig
 	[w h] [(count v) (-> v first count)]]
-    (do-board [w h]  (aset! ints v i j (if (>= j (/ h 2)) -1 1)))
+    (do-board [w h]  (aset! doubles v i j (if (>= j (/ h 2)) -0.5 0.5)))
     [u v]))
 
 (defn randr-array [orig]
   (let [[u v] orig
 	[w h] [(count v) (-> v first count)]]
     (do-board [w h]
-	      (aset! ints u i j (rand-int 2))
-	      (aset! ints v i j (rand-int 2)))
+	      (aset! doubles u i j (- 1.0 (rand 2)))
+	      (aset! doubles v i j (- 1.0 (rand 2))))
     [u v]))
 
 (defn show-board [b]
@@ -114,81 +100,121 @@
 	state
 	(recur (dec n) next-state)))))
 
+(defn set-bounds [arr b]
+  (let [N      (int (- (count arr) (int 2)))
+	n1     (unchecked-inc N)	
+	cswap! (fn [#^Integer x #^Integer y old p]
+		 (aset! doubles arr x y (if p (- old) old)))]
+    (loop [i (int 1)]
+      (when (<= i N)
+	(cswap! 0  i (aget! doubles arr 1 i) (= b 1))
+	(cswap! n1 i (aget! doubles arr N i) (= b 1))
+	(cswap! i  0 (aget! doubles arr i 1) (= b 2))
+	(cswap! i n1 (aget! doubles arr i N) (= b 2))
+	(recur (unchecked-inc i))))
+    (aset! doubles arr (int 0) (int 0)
+	   (* 0.5 (+ (aget! doubles arr 1 0)
+		     (aget! doubles arr 0 1))))
+    (aset! doubles arr (int 0) n1
+	    (* 0.5 (+ (aget! doubles arr 1 n1)
+		      (aget! doubles arr 0 N))))
+    (aset! doubles arr n1 (int 0)
+	    (* 0.5 (+ (aget! doubles arr N 0)
+		      (aget! doubles arr n1 1))))
+    (aset! doubles arr n1 n1
+	    (* 0.5 (+ (aget! doubles arr N n1)
+		      (aget! doubles arr n1 N))))
+    arr))
+
+(defmacro map! [f lookup collection]
+  `(let [[w# h#] [(count ~lookup) (-> ~lookup first count)]]
+     (let [board# (make-array Double/TYPE h# w#)]
+       (do-board [w# h#]       
+	    (let [~'above  (aget! ~'doubles ~lookup (unchecked-dec ~'i) ~'j)
+		  ~'below  (aget! ~'doubles ~lookup (unchecked-inc ~'i) ~'j)
+		  ~'left   (aget! ~'doubles ~lookup ~'i (unchecked-dec ~'j))
+		  ~'right  (aget! ~'doubles ~lookup ~'i (unchecked-inc ~'j))
+		  ~'self   (aget! ~'doubles ~lookup ~'i ~'j)]
+	      (aset! ~'doubles board# (int ~'i) (int ~'j) ~f)))
+       board#)))
+
 (defn diffuse-board
-  [board diffusion]
+  [board b diffusion]
   (let [sz         (count board)
 	dr         (* *dt* diffusion sz sz)
-	dconst     (* 4 dr)]
-    (map! (/ (+ self (* dr (+ (+ above below) (+ left right)))) dconst)
-	  board (make-array Double/TYPE sz sz))))
+	dconst     (* 4 dr)
+	d1         (new-array sz sz)]
+   (-> (map! (/ (+ self (* dr (+ (+ above below) (+ left right)))) dconst)
+	     board d1)
+	(set-bounds b))))
 
 (defn advection [densities velocities]
   (let [[w h] [(count densities) (-> densities first count)]
 	[u v] velocities
-	retr  (make-densities w h)
+	retr  (new-array w h)
 	[w h] [(unchecked-dec w) (unchecked-dec h)]]
     (do-board [w h]
-      (let [x  (- i (* *dt* (aget! ints u i j)))
-	    y  (- j (* *dt* (aget! ints v i j)))
+      (let [x  (- i (* *dt* (aget! doubles u i j)))
+	    y  (- j (* *dt* (aget! doubles v i j)))
 	    x  (cond (< x 0.5)       0.5
-		     (> x (+ 0.5 w)) (+ 0.5 w)
+		     (> x (+ 0.5 w)) (+ 0.5 (unchecked-dec w))
 		     :else x)
 	    y  (cond (< y 0.5)       0.5
 		     (> y (+ 0.5 h)) (+ 0.5 (unchecked-dec h))
 		     :else y)
-
-	    i0 (int x)
-	    i1 (unchecked-inc i0)
-
-	    j0 (int y)
-	    j1 (unchecked-inc j0)
-
-	    s1 (int (- x i0))
-	    s0 (int (- 1 s1))
-
-	    t1 (int (- y j0))
-	    t0 (int (- 1 t1))]
+	    i0  (int x)
+	    i1  (unchecked-inc i0)
+	    j0  (int y)
+	    j1  (unchecked-inc j0)
+	    s1  (- x i0)
+	    s0  (- 1 s1)
+	    t1  (- y j0)
+	    t0  (- 1 t1)]
 	(aset! doubles retr i j
 		   (+ (* s0 (+ (* t0 (aget! doubles densities i0 j0))
 			       (* t1 (aget! doubles densities i0 j1))))
 		      (* s1 (+ (* t0 (aget! doubles densities i1 j0))
 			       (* t1 (aget! doubles densities i1 j1))))))))
-    retr))		    
+    retr))
 
-(defn set-bounds [velocities b idx]
-  velocities)		
-
+      
 (defn project [velocities]
-  (let [[w h]    [(count velocities) (-> velocities first count)]
-	[u v]    velocities
+  (let [[u v]    velocities
+	[w h]    [(count u) (-> u first count)]
 	r        (/ 1.0 h)
-	p        (make-densities w h)
-	div      (make-densities w h)]
+	p        (new-array w h)
+	div      (new-array w h)
+	half     (double 0.5)]
     (do-board [w h]
        (aset! doubles div i j
-		  (* -0.5 r (+ (- (aget! ints u (unchecked-inc i) j)
-				  (aget! ints u (unchecked-dec i) j))
-			       (- (aget! ints v i (unchecked-inc j))
-				  (aget! ints v i (unchecked-dec j)))))))
+		  (* -0.5 r (+ (- (aget! doubles u (unchecked-inc i) j)
+				  (aget! doubles u (unchecked-dec i) j))
+			       (- (aget! doubles v i (unchecked-inc j))
+				  (aget! doubles v i (unchecked-dec j)))))))
+    (set-bounds div 0)
+    (set-bounds p 0)
     (dotimes [k 20]
+      (do-board [w h]
+		(aset! doubles p i j
+		 (/ (+ (+ (aget! doubles div i j)
+			  (aget! doubles p (unchecked-dec i) j))
+		       (+ (aget! doubles p (unchecked-inc i) j)
+			  (aget! doubles p i (unchecked-dec j)))
+		       (aget! doubles p i (unchecked-inc j))) (int 4))))
+      (set-bounds p 0))
     (do-board [w h]
-      (aset! doubles p
-		 (/ (+ (aget! doubles div i j)
-		       (aget! doubles p (unchecked-dec i) j)
-		       (aget! doubles p (unchecked-inc i) j)
-		       (aget! doubles p i (unchecked-dec j))
-		       (aget! doubles p i (unchecked-inc j))) 4))))
-    (do-board [w h]
-      (aset! ints u i j
-	     (- (aget! ints u i j)
-		(/ (* 0.5 (- (aget! doubles p (unchecked-inc i) j)
-			     (aget! doubles p (unchecked-dec i) j)))
+      (aset! doubles u i j
+	     (- (aget! doubles u i j)
+		(/ (* half (- (aget! doubles p (unchecked-inc i) j)
+			      (aget! doubles p (unchecked-dec i) j)))
 		   r)))
-      (aset! ints v i j
-	     (- (aget! ints v i j)
-		(/ (* 0.5 (- (aget! doubles p i (unchecked-inc j))
+      (aset! doubles v i j
+	     (- (aget! doubles v i j)
+		(/ (* half (- (aget! doubles p i (unchecked-inc j))
 			     (aget! doubles p i (unchecked-dec j))))
 		   r))))
+    (set-bounds u 1)
+    (set-bounds v 2)
     [u v]))
     
 ;; Rendering
@@ -208,46 +234,40 @@
 	(let [[u v] velocities
 	      mx    (+ (* i scale) (/ scale 2)) ; Middle of cell, X
 	      my    (+ (* j scale) (/ scale 2))
-	      ux    (aget! ints u i j)
-	      vy    (aget! ints v i j)] 
+	      ux    (aget! doubles u i j)
+	      vy    (aget! doubles v i j)] 
 	  (if (pos? vy)
 	    (.setColor g Color/RED)
 	    (.setColor g Color/GREEN))
 	(.drawLine g mx my (dec (+ mx (* ux scale)))
 		           (dec (+ my (* vy scale)))))))))
 
-(defmacro defworker [name args & body]
-  " Defines a worker which discontinues looping his main body
-    if the global atom 'running' is non-true. Exceptions are
-    caught and printed "
-  `(defn ~name ~args
-     (while @running
-	    (try ~@body (catch Exception e#
-			  (-> e# .getMessage println))))))
+(defn density-step [d v sources]
+  (when-let [s (seq @sources)] (swap! d add-sources s))
+  (swap! d diffuse-board 0 1)
+  (swap! d advection v))
 
-(defworker animator [panel]
-  (.repaint panel)
-  (Thread/sleep 40))
+(defn velocity-step [velocities]
+  (let [[u v] @velocities]
+    (reset! velocities [(diffuse-board v 1 1) (diffuse-board u 1 1)])
+    (swap!  velocities project)
+    (reset! velocities [(advection u @velocities) (advection v @velocities)])
+    (swap!  velocities project)))
 
-(defworker density-step [densities velocities sources panel]
-  (when-let [s (seq @sources)] (swap! densities add-sources s))
-  (swap! densities diffuse-board 4)
-  (swap! densities advection @velocities)
-
-  (when-let [s (seq @sources)] (swap! densities add-sources s))
-  (swap! densities diffuse-board 4)
-  (swap! velocities project)
-  (swap! densities advection @velocities)
-  (swap! velocities project))
+(defn dynamics [densities velocities sources panel]
+  (while @running
+	 (velocity-step velocities)
+	 (density-step  densities @velocities sources)
+	 (.repaint panel)))
   
 (defn new-source [e scale sources]
   (swap! sources conj [(int (/ (.getX e) scale)) (int (/ (.getY e) scale))]))
 
 (defn main [scale [w h]]
   (reset! running true)
-  (let [densities  (atom (make-densities  w h))
+  (let [densities  (atom (new-array  w h))
 	d0         (make-array Double/TYPE w h)
-	velocities (atom (randr-array [(new-array w h) (new-array w h)]))
+	velocities (atom (topdown-array [(new-array w h) (new-array w h)]))
 	sources    (atom [])
 	panel (doto (proxy [JPanel] []
 		      (paint [g] (render-scene g w h scale
@@ -270,6 +290,4 @@
       .pack .show
       (.setLocation 400 200)
       (.add panel))
-    (future (animator panel))
-    (future (density-step densities velocities sources panel))
-));    (future (velocity-step densities velocities))))
+    (future (dynamics densities velocities sources panel))))
