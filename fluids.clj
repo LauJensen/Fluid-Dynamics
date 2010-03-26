e6a57af1e0ed0dbd9c4f0bd03ca7f7f73b9228f7
Lau B. Jensen <lau.jensen@bestinclass.dk>
2010-03-26 13:31:59 +0100

    Simplified aget! and aset! 5msecs faster on a 1000x1000 board

--- fluids.clj
+++ fluids.clj
@@ -7,24 +7,27 @@
 
 ;; Helpers
 
-(defmacro aget!
-  ([hint array idx]
-     `(aget ~(vary-meta array assoc :tag hint) ~idx))
-  ([hint array idx & idxs]
-     `(let [a# (aget ~(vary-meta array assoc :tag 'objects) ~idx)]	
-	(aget! ~hint a# ~@idxs))))
+;(defn aset! [arr #^Integer x #^Integer y v]
+ ; (let [#^doubles f (aget #^objects arr x)]
+  ;  (aset f y (double v))))
 
-(defmacro aset! [hint array & idxsv]
-  (let [hints '{doubles double ints int}
-        [v idx & sxdi] (reverse idxsv)
-        idxs (reverse sxdi)
-        v (if-let [h (hints hint)] (list h v) v)
-        nested-array (if (seq idxs)
-                       `(aget! ~'objects ~array ~@idxs)
-                        array)
-        a-sym (with-meta (gensym "a") {:tag hint})]
+(defmacro aget!
+  ([array y]      `(aget ~(vary-meta array assoc :tag 'doubles) ~y))
+  ([array x & ys] `(let [a# (aget ~(vary-meta array assoc :tag 'objects) ~@ys)]
+		     (aget! a# ~x))))
+
+(defmacro aset! [array x y v]
+  (let [nested-array `(aget ~(vary-meta array assoc :tag 'objects) ~y)
+        a-sym         (with-meta (gensym "a") {:tag 'doubles})]
     `(let [~a-sym ~nested-array]
-       (aset ~a-sym ~idx ~v))))
+       (aset ~a-sym ~x (double ~v)))))
+
+(defn new-array [w h]
+  (let [board (make-array Double/TYPE h w)]
+    (dotimes [i w]
+      (dotimes [j h]
+	(aset! board i j 0.0)))
+    board))
 
 (defmacro do-board [[w h] & body]
   `(let [w# (int (dec ~w)) h# (int (dec ~h))]
@@ -35,36 +38,29 @@
 	     (do ~@body)
 	     (recur (unchecked-inc ~'j))))
 	 (recur (unchecked-inc ~'i))))))
-
 (declare diffuse-board)
 
 (def *dt* 1.0)
 
-(defn new-array [w h]
-  (let [board (make-array Double/TYPE h w)]
-    (dotimes [i w]
-      (dotimes [j h]
-	(aset! doubles board i j 0.0)))
-    board))
 
 (defn up-array [orig]
   (let [[u v] orig
 	[w h] [(count v) (-> v first count)]]
-    (do-board [w h]  (aset! doubles v i j -0.5))
+    (do-board [w h]  (aset! v i j -0.5))
     [u v]))
 
 (defn topdown-array [orig]
   (let [[u v] orig
 	[w h] [(count v) (-> v first count)]]
-    (do-board [w h]  (aset! doubles v i j (if (>= j (/ h 2)) -0.5 0.5)))
+    (do-board [w h]  (aset! v i j (if (>= j (/ h 2)) -0.5 0.5)))
     [u v]))
 
 (defn randr-array [orig]
   (let [[u v] orig
 	[w h] [(count v) (-> v first count)]]
     (do-board [w h]
-	      (aset! doubles u i j (- 1.0 (rand 2)))
-	      (aset! doubles v i j (- 1.0 (rand 2))))
+	      (aset! u i j (- 1.0 (rand 2)))
+	      (aset! v i j (- 1.0 (rand 2))))
     [u v]))
 
 (defn show-board [b]
@@ -88,7 +84,7 @@
 
 (defn add-sources
   [board sources]
-  (doseq [[x y] sources] (aset! doubles board x y 255.0))
+  (doseq [[x y] sources] (aset! board x y 255.0))
   board)
 	
 ;; Diffusion
@@ -97,26 +93,26 @@
   (let [N      (int (- (count arr) (int 2)))
 	n1     (unchecked-inc N)	
 	cswap! (fn [#^Integer x #^Integer y old p]
-		 (aset! doubles arr x y (if p (- old) old)))]
+		 (aset! arr x y (if p (- old) old)))]
     (loop [i (int 1)]
       (when (<= i N)
-	(cswap! 0  i (aget! doubles arr 1 i) (= b 1))
-	(cswap! n1 i (aget! doubles arr N i) (= b 1))
-	(cswap! i  0 (aget! doubles arr i 1) (= b 2))
-	(cswap! i n1 (aget! doubles arr i N) (= b 2))
+	(cswap! 0  i (aget! arr 1 i) (= b 1))
+	(cswap! n1 i (aget! arr N i) (= b 1))
+	(cswap! i  0 (aget! arr i 1) (= b 2))
+	(cswap! i n1 (aget! arr i N) (= b 2))
 	(recur (unchecked-inc i))))
-    (aset! doubles arr (int 0) (int 0)
-	   (* 0.5 (+ (aget! doubles arr 1 0)
-		     (aget! doubles arr 0 1))))
-    (aset! doubles arr (int 0) n1
-	    (* 0.5 (+ (aget! doubles arr 1 n1)
-		      (aget! doubles arr 0 N))))
-    (aset! doubles arr n1 (int 0)
-	    (* 0.5 (+ (aget! doubles arr N 0)
-		      (aget! doubles arr n1 1))))
-    (aset! doubles arr n1 n1
-	    (* 0.5 (+ (aget! doubles arr N n1)
-		      (aget! doubles arr n1 N))))
+    (aset! arr (int 0) (int 0)
+	   (* 0.5 (+ (aget! arr 1 0)
+		     (aget! arr 0 1))))
+    (aset! arr (int 0) n1
+	    (* 0.5 (+ (aget! arr 1 n1)
+		      (aget! arr 0 N))))
+    (aset! arr n1 (int 0)
+	    (* 0.5 (+ (aget! arr N 0)
+		      (aget! arr n1 1))))
+    (aset! arr n1 n1
+	    (* 0.5 (+ (aget! arr N n1)
+		      (aget! arr n1 N))))
     arr))
 
 (defmacro map! [f lookup collection]
@@ -124,12 +120,12 @@
      (let [board# (make-array Double/TYPE h# w#)]
        (dotimes [k# (int 20)]
 	 (do-board [w# h#]       
-	    (let [~'above  (aget! ~'doubles ~lookup (unchecked-dec ~'i) ~'j)
-		  ~'below  (aget! ~'doubles ~lookup (unchecked-inc ~'i) ~'j)
-		  ~'left   (aget! ~'doubles ~lookup ~'i (unchecked-dec ~'j))
-		  ~'right  (aget! ~'doubles ~lookup ~'i (unchecked-inc ~'j))
-		  ~'self   (aget! ~'doubles ~lookup ~'i ~'j)]
-	      (aset! ~'doubles board# (int ~'i) (int ~'j) ~f)))
+	    (let [~'above  (aget! ~lookup (unchecked-dec ~'i) ~'j)
+		  ~'below  (aget! ~lookup (unchecked-inc ~'i) ~'j)
+		  ~'left   (aget! ~lookup ~'i (unchecked-dec ~'j))
+		  ~'right  (aget! ~lookup ~'i (unchecked-inc ~'j))
+		  ~'self   (aget! ~lookup ~'i ~'j)]
+	      (aset! board# (int ~'i) (int ~'j) ~f)))
 	 (set-bounds board# 0))
        board#)))
 
@@ -148,8 +144,8 @@
 	retr  (new-array w h)
 	[w h] [(unchecked-dec w) (unchecked-dec h)]]
     (do-board [w h]
-      (let [x  (- i (* *dt* (aget! doubles u i j)))
-	    y  (- j (* *dt* (aget! doubles v i j)))
+      (let [x  (- i (* *dt* (aget! u i j)))
+	    y  (- j (* *dt* (aget! v i j)))
 	    x  (cond (< x 0.5)       0.5
 		     (> x (+ 0.5 w)) (+ 0.5 (unchecked-dec w))
 		     :else x)
@@ -164,11 +160,11 @@
 	    s0  (- 1 s1)
 	    t1  (- y j0)
 	    t0  (- 1 t1)]
-	(aset! doubles retr i j
-		   (+ (* s0 (+ (* t0 (aget! doubles densities i0 j0))
-			       (* t1 (aget! doubles densities i0 j1))))
-		      (* s1 (+ (* t0 (aget! doubles densities i1 j0))
-			       (* t1 (aget! doubles densities i1 j1))))))))
+	(aset! retr i j
+		   (+ (* s0 (+ (* t0 (aget! densities i0 j0))
+			       (* t1 (aget! densities i0 j1))))
+		      (* s1 (+ (* t0 (aget! densities i1 j0))
+			       (* t1 (aget! densities i1 j1))))))))
     (set-bounds retr b)
     retr))
 
@@ -181,32 +177,32 @@
 	div      (new-array w h)
 	half     (double 0.5)]
     (do-board [w h]
-       (aset! doubles div i j
-		  (* -0.5 r (+ (- (aget! doubles u (unchecked-inc i) j)
-				  (aget! doubles u (unchecked-dec i) j))
-			       (- (aget! doubles v i (unchecked-inc j))
-				  (aget! doubles v i (unchecked-dec j)))))))
+       (aset! div i j
+		  (* -0.5 r (+ (- (aget! u (unchecked-inc i) j)
+				  (aget! u (unchecked-dec i) j))
+			       (- (aget! v i (unchecked-inc j))
+				  (aget! v i (unchecked-dec j)))))))
     (set-bounds div 0)
     (set-bounds p 0)
     (dotimes [k 20]
       (do-board [w h]
-		(aset! doubles p i j
-		 (/ (+ (+ (aget! doubles div i j)
-			  (aget! doubles p (unchecked-dec i) j))
-		       (+ (aget! doubles p (unchecked-inc i) j)
-			  (aget! doubles p i (unchecked-dec j)))
-		       (aget! doubles p i (unchecked-inc j))) (int 4))))
+		(aset! p i j
+		 (/ (+ (+ (aget! div i j)
+			  (aget! p (unchecked-dec i) j))
+		       (+ (aget! p (unchecked-inc i) j)
+			  (aget! p i (unchecked-dec j)))
+		       (aget! p i (unchecked-inc j))) (int 4))))
       (set-bounds p 0))
     (do-board [w h]
-      (aset! doubles u i j
-	     (- (aget! doubles u i j)
-		(/ (* half (- (aget! doubles p (unchecked-inc i) j)
-			      (aget! doubles p (unchecked-dec i) j)))
+      (aset! u i j
+	     (- (aget! u i j)
+		(/ (* half (- (aget! p (unchecked-inc i) j)
+			      (aget! p (unchecked-dec i) j)))
 		   r)))
-      (aset! doubles v i j
-	     (- (aget! doubles v i j)
-		(/ (* half (- (aget! doubles p i (unchecked-inc j))
-			     (aget! doubles p i (unchecked-dec j))))
+      (aset! v i j
+	     (- (aget! v i j)
+		(/ (* half (- (aget! p i (unchecked-inc j))
+			     (aget! p i (unchecked-dec j))))
 		   r))))
     (set-bounds u 1)
     (set-bounds v 2)
@@ -222,15 +218,15 @@
     (.setColor Color/BLACK)
     (.fillRect 0 0 (* scale w) (* scale h)))
   (do-board [w h]
-    (let [density (min 255 (int (aget! doubles board i j)))]
+    (let [density (min 255 (int (aget! board i j)))]
       (.setColor g (Color. density density density))
       (.fillRect g (* i scale) (* j scale) scale scale)
       (when @show-velocities
 	(let [[u v] velocities
 	      mx    (+ (* i scale) (/ scale 2)) ; Middle of cell, X
 	      my    (+ (* j scale) (/ scale 2))
-	      ux    (aget! doubles u i j)
-	      vy    (aget! doubles v i j)] 
+	      ux    (aget! u i j)
+	      vy    (aget! v i j)] 
 	  (if (pos? vy)
 	    (.setColor g Color/RED)
 	    (.setColor g Color/GREEN))
@@ -240,7 +236,7 @@
 (defn density-step [d v sources]
   (when-let [s (seq @sources)] (swap! d add-sources s))
   (swap! d diffuse-board 0 1)
-  (swap! d advection v 0))
+);  (swap! d advection v 0))
 
 (defn velocity-step [velocities]
   (let [[u v] @velocities]
@@ -253,7 +249,7 @@
 
 (defn dynamics [densities velocities sources panel]
   (while @running
-	 (velocity-step velocities)
+;	 (velocity-step velocities)
 	 (density-step  densities @velocities sources)
 	 (.repaint panel)))
   
